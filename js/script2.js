9bfe2abcd3ee24cea1196f89021ae3e213a44401
francismarion <francismarion.abello.iics@ust.edu.ph>
2021-09-14 11:28:33 +0800

    added script2.js

---
+++ js/script2.js
@@ -0,0 +1,14 @@
+function greet(name,message)
+{
+
+    if(name === "Juan Dela Cruz" && message === "Good Morning")
+    {
+        return "Good morning Juan!";
+    }
+    else
+    {
+        return null;
+    }
+}
+
+console.log(greet("Juan Dela Cruz","Good Morning"));
