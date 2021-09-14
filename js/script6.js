e3ef1ddcd66231e5ffce4ae057a5f088eb53d7ee
francismarion <francismarion.abello.iics@ust.edu.ph>
2021-09-14 11:52:38 +0800

    added script4.js

---
+++ js/script6.js
@@ -0,0 +1,13 @@
+class Triangle
+{                                  
+    constructor(side1, side2, side3)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+    computePerimeter()
+    {
+        return this.side1 + this.side2 + this.side3;
+    }
+}
